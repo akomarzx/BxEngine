b9c270cc38ae924b7b93243254680555f2592c2e
Akomarz Ronald <ronaldjr.ombao@gmail.com>
2021-04-22 08:39:23 -0400

    None

--- header/GDebugMessageCallBack.h
+++ header/GDebugMessageCallBack.h
@@ -2,6 +2,8 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 
+
+
 void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
 	GLenum severity, GLsizei length,
 	const GLchar* msg, const void* data);
